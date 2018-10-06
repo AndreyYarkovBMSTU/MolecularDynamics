d63f2cb6dc8b24eb4bcb3ae31278654fcca9693d
AndreyYarkovBMSTU <43884585+AndreyYarkovBMSTU@users.noreply.github.com>
2018-10-06 13:22:05 +0000

    Add files via upload

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include "Eigen/Eigen"
+#include "namespase/phys.h"
+#include "namespase/mathematics.h"
+//#include "properties.h"
+
+using namespace phys;
+using namespace std;
+
+typedef Eigen::Vector3d Vector;
+typedef Eigen::MatrixXd Matrix;
+
+#endif // HEADER_H
